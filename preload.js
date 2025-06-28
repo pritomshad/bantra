d1b3bbd0a08ab1f513c11b155dc2a83a6c7961b9
pritomshad <2021331039@student.sust.edu>
2025-06-28 10:44:20 +0600

    Stop-transcription now closes caption window and kills asr.py python process

--- preload.js
+++ preload.js
@@ -1,6 +1,8 @@
-const { contextBridge, ipcRenderer } = require('electron/renderer')
+const { contextBridge, ipcRenderer } = require("electron/renderer");
 
-contextBridge.exposeInMainWorld('bantraAPI', {
-    onPythonStdOut: (callback) => ipcRenderer.on('py-message', (_event, value) => callback(value)),
-    onTranscriptionButtonPress: () => ipcRenderer.send('start-transcription')
-})
+contextBridge.exposeInMainWorld("bantraAPI", {
+  onPythonStdOut: (callback) =>
+    ipcRenderer.on("py-message", (_event, value) => callback(value)),
+  onStartTranscription: () => ipcRenderer.send("start-transcription"),
+  onStopTranscription: () => ipcRenderer.send("stop-transcription")
+});
--- renderer.js
+++ renderer.js
@@ -1,6 +1,20 @@
 // Transcription button press
-const transcripButton = document.getElementById('transciption-button')
+const transcripButton = document.getElementById("transciption-button");
 
-transcripButton.addEventListener('click', () => {
-    window.bantraAPI.onTranscriptionButtonPress()
-})
+transcripButton.addEventListener("click", () => {
+  if (transcripButton.value == "1") {
+    transcripButton.innerText = "STOP";
+    transcripButton.value = "0";
+    window.bantraAPI.onStartTranscription();
+  } else {
+    transcripButton.innerText = "START TRANSCRIPTION";
+    transcripButton.value = "1";
+
+    // some function to process the transcript and save the note
+
+    /**
+     * On stop transcription the caption window is closed and python process is killed
+     */
+    window.bantraAPI.onStopTranscription();
+  }
+});
