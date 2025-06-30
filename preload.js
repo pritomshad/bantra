e2aa6253dd2003be576ce13294cf250ed34fbaa4
pritomshad <2021331039@student.sust.edu>
2025-07-01 01:42:48 +0600

    llama3.2 added for note generation

--- preload.js
+++ preload.js
@@ -1,10 +1,12 @@
 const { contextBridge, ipcRenderer } = require("electron/renderer");
 
-contextBridge.exposeInMainWorld("bantraAPI", {
-  onPythonStdOut: (callback) =>
-    ipcRenderer.on("py-message", (_event, value) => callback(value)),
+contextBridge.exposeInMainWorld('bantraAPI', {
+  onPythonStdOut: (callback) => 
+    ipcRenderer.on('py-message', (_event, value) => callback(value)),
   onTranscriptionButtonPress: (k) => {
     if (k == 1) ipcRenderer.send("start-transcription");
     else ipcRenderer.send("stop-transcription");
   },
-});
+  onInference: (callback) => ipcRenderer.on("inference", (_event, token) => callback(token)),
+  onInferenceDone: (callback) => ipcRenderer.on("inference-done", () => callback())
+})
